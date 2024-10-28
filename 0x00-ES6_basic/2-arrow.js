950e441a0ae4cbaf76eb9c6cdc52d29c0bf2089b
ScottShadow <rudaseswascottmc@gmail.com>
2024-10-28 15:07:57 +0200

    added files

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * Returns a list of San Francisco neighborhoods.
+ *
+ * @return {Array} A list of San Francisco neighborhoods.
+ */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
